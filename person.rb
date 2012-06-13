d7fa0da37a062c645362e0ac4b8caa66c36fb0fd
Jen Dockter <jendockter@gmail.com>
2012-06-12 17:22:35 -0700

    added some underscores

--- person.rb
+++ person.rb
@@ -1,13 +1,9 @@
 class Person
-  attr_accessor :firstname, :lastname, :age
-    
-  def greet
-    return "your name is #{firstname} #{lastname} and you are #{age} years old."
-  end
-  
-  def initialize(firstname, lastname, age)
-    @firstname = firstname
-    @lastname = lastname
+  attr_accessor :first_name, :last_name, :age
+      
+  def initialize(first_name, last_name, age)
+    @first_name = first_name
+    @last_name = last_name
     @age = age
   end
   
